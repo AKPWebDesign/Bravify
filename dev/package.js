81172adaa5580c99faee97d2e718fc53f5621a94
Austin Peterson <austin@akpwebdesign.com>
2016-03-09 11:55:39 -0600

    build script is finished... for now.

--- dev/package.js
+++ dev/package.js
@@ -1,11 +1,13 @@
 var pkg = require('../package.json');
+var path = require('path');
+var rcedit = require('rcedit');
 var moment = require('moment');
 var packager = require('electron-packager');
 var copyright = `Copyright ${moment().format('YYYY')} ${pkg.author}. All Rights Reserved.`;
 var opts = {
   "arch": "all",
   "dir": "./",
-  "platform": "win32,darwin",
+  "platform": "win32",
   "app-copyright": copyright,
   "app-version": pkg.version,
   "asar": true,
@@ -16,6 +18,34 @@ var opts = {
   "overwrite": true,
   "version": "0.36.9"
 };
+
 packager(opts, function(err, appPath) {
-  if(err) {console.error(err);} else {console.log(appPath);}
+  if(err) {console.error(err);} else {console.log(appPath); doDarwinBuild(); doRcEdit(appPath)}
 });
+
+function doDarwinBuild() {
+  opts.platform = "darwin";
+  packager(opts, function(err, appPath) {
+    if(err) {console.error(err);} else {console.log(appPath);}
+  });
+}
+
+function doRcEdit(appPath) {
+  var rcOpts = {
+    "version-string": {
+      "ProductName": "Bravify",
+      "FileDescription": "Bravify",
+      "CompanyName": "AKPWebDesign",
+      "LegalCopyright": copyright,
+    },
+    "file-version": pkg.version,
+    "product-version": pkg.version
+    //"icon": ""
+  }
+  for (var i = 0; i < appPath.length; i++) {
+    var pathStr = path.join(appPath[i], "Bravify.exe");
+    rcedit(pathStr, rcOpts, function(err){
+      if(err){console.log(err);} else {console.log("rcedit finished on " + pathStr);}
+    });
+  }
+}
