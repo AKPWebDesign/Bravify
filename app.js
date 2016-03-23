4535c254baf79c6e71dddd9022c4e106e4edc5fc
Austin Peterson <austin@akpwebdesign.com>
2016-03-23 14:04:59 -0500

    change app closing slightly.

--- app.js
+++ app.js
@@ -30,11 +30,7 @@ var champSelectWindow = null;
 new Promise(function(resolve){
   // Quit when all windows are closed.
   app.on('window-all-closed', function() {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
+    app.quit();
   });
 
   // This method will be called when Electron has finished
@@ -65,13 +61,6 @@ new Promise(function(resolve){
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      mainWindow = null;
-      if(champSelectWindow) {
-        champSelectWindow.close();
-      }
       app.quit();
     });
   });
