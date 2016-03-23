f6a2099077d64065ddf4fc878622dc9906ea1f63
Austin Peterson <austin@akpwebdesign.com>
2016-03-22 22:33:14 -0500

    Start on autoupdate script.

---
+++ API/Autoupdate.js
@@ -0,0 +1,12 @@
+var Promise = require('bluebird'); // jshint ignore:line
+
+module.exports = new Promise(function(resolve, reject) {
+  resolve(true);
+
+  //check for updated version
+
+    //download updated app.asar
+    //restart
+
+  //resolve true if no update required
+});
--- app.js
+++ app.js
@@ -18,86 +18,140 @@ const jsonfile = require('jsonfile'); //tools for saving JSON to files.
 const mkdirp = require('mkdirp'); //recursive mkdir
 const homedir = require('homedir'); //home directory finder
 
-// Load data from Riot APIs when we start the application.
-const APIData = new (require('./API/APIData'))(getPrefDir());
-const BuildGenerator = new (require('./API/BuildGenerator'))(APIData);
-const ItemSetGenerator = new (require('./API/ItemSetGenerator'))();
+var APIData, BuildGenerator, ItemSetGenerator;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
-var champSelectWindow = null;
+//run autoupdater first thing.
+require('./API/Autoupdate').then(function() {
+  // Load data from Riot APIs when we start the application.
+  APIData = new (require('./API/APIData'))(getPrefDir());
+  BuildGenerator = new (require('./API/BuildGenerator'))(APIData);
+  ItemSetGenerator = new (require('./API/ItemSetGenerator'))();
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  // Keep a global reference of the window object, if you don't, the window will
+  // be closed automatically when the JavaScript object is garbage collected.
+  var mainWindow = null;
+  var champSelectWindow = null;
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1050,
-    height: 600,
-    'min-width': 1050,
-    'min-height': 600,
-    fullscreen: false,
-    center: true,
-    resizable: true,
-    show: false,
-    frame: false,
-    transparent: true,
-    icon: __dirname + '/resources/icon.png',
-    title: 'Bravify'});
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/html/index.html');
-
-  mainWindow.webContents.on('did-finish-load', function(){
-    mainWindow.show();
-    //begin loading API data.
-    loadData();
-  });
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-    if(champSelectWindow) {
-      champSelectWindow.close();
+  // Quit when all windows are closed.
+  app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
     }
+  });
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  app.on('ready', function() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+      width: 1050,
+      height: 600,
+      'min-width': 1050,
+      'min-height': 600,
+      fullscreen: false,
+      center: true,
+      resizable: true,
+      show: false,
+      frame: false,
+      transparent: true,
+      icon: __dirname + '/resources/icon.png',
+      title: 'Bravify'});
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/html/index.html');
+
+    mainWindow.webContents.on('did-finish-load', function(){
+      mainWindow.show();
+      //begin loading API data.
+      loadData(mainWindow);
+    });
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null;
+      if(champSelectWindow) {
+        champSelectWindow.close();
+      }
+      app.quit();
+    });
+  });
+
+  // Called when the client requests a new build to be generated
+  ipcMain.on('generateNewBuild', function(event, message) {
+    BuildGenerator.generate(message).then(function(result) {
+      mainWindow.webContents.send('buildGenerated', result);
+    });
+  });
+
+  ipcMain.on('saveBuild', function(event, message) {
+    if(message.build) {
+      var set = ItemSetGenerator.generate(message.build);
+      saveBuild(set);
+      mainWindow.webContents.send('itemSetSaved', set);
+    }
+  });
+
+  ipcMain.on('deleteBuild', function() {
+    deleteBuild();
+  });
+
+  ipcMain.on('changeLeaguePath', function() {
+    getLeaguePath(false);
+  });
+
+  // The methods below will be called upon receiving various messages from our
+  // rendering thread. These are used to do things in the app when the user clicks
+  // on things in the interface.
+  ipcMain.on('close-main-window', function() {
     app.quit();
   });
-});
 
-// Called when the client requests a new build to be generated
-ipcMain.on('generateNewBuild', function(event, message) {
-  BuildGenerator.generate(message).then(function(result) {
-    mainWindow.webContents.send('buildGenerated', result);
+  ipcMain.on('minimize-main-window', function() {
+    mainWindow.minimize();
   });
-});
 
-ipcMain.on('saveBuild', function(event, message) {
-  if(message.build) {
-    var set = ItemSetGenerator.generate(message.build);
-    saveBuild(set);
-    mainWindow.webContents.send('itemSetSaved', set);
-  }
-});
+  ipcMain.on('maximize-main-window', function() {
+    (mainWindow.isMaximized() ? mainWindow.unmaximize() : mainWindow.maximize()); // jshint ignore:line
+  });
 
-ipcMain.on('deleteBuild', function() {
-  deleteBuild();
-});
+  ipcMain.on('openChampSelect', function() {
+    champSelectWindow = openWindow(champSelectWindow);
+  });
 
-ipcMain.on('changeLeaguePath', function() {
-  getLeaguePath(false);
+  ipcMain.on('retrieveChamps', function() {
+    if(!champSelectWindow) {return;}
+    champSelectWindow.webContents.send('champions', {champs: APIData.champs, versions: APIData.versionData, groups: require('./API/groups.json'), selected: APIData.champKeys});
+  });
+
+  ipcMain.on('newChampSelection', function(event, champs) {
+    if(!champs) {return;}
+    APIData.champKeys = champs;
+    mainWindow.webContents.send('champsChanged');
+  });
+
+  ipcMain.on('closeChampsWindow', function() {
+    if(!champSelectWindow) {return;}
+    champSelectWindow.close();
+  });
+
+  ipcMain.on('reloadData', function() {
+    loadData(mainWindow);
+  });
+
+  ipcMain.on('openURL', function(event, message) {
+    var open = require('open');
+    if(message) {
+      open(message);
+    }
+  });
+}, function(err) {
+  dialog.showErrorBox('Bravify Error!', 'An error has occurred:\n' + err);
+  process.exit(1);
 });
 
 function deleteBuild() {
@@ -257,9 +311,9 @@ function getPrefDir() {
   return dir;
 }
 
-function openChampSelectWindow() {
-  if(!champSelectWindow) {
-    champSelectWindow = new BrowserWindow({
+function openWindow(window) {
+  if(!window) {
+    window = new BrowserWindow({
       width: 557,
       height: 647,
       fullscreen: false,
@@ -271,29 +325,31 @@ function openChampSelectWindow() {
       icon: __dirname + '/resources/icon.png',
       title: 'Bravify Champ Select'});
 
-    champSelectWindow.loadURL('file://' + __dirname + '/html/champions.html');
+    window.loadURL('file://' + __dirname + '/html/champions.html');
 
-    champSelectWindow.webContents.on('did-finish-load', function(){
-      champSelectWindow.show();
+    window.webContents.on('did-finish-load', function(){
+      window.show();
     });
 
-    champSelectWindow.on('closed', function(){
-      champSelectWindow = null;
+    window.on('closed', function(){
+      window = null;
     });
   } else {
-      champSelectWindow.close();
+      window.close();
   }
+
+  return window;
 }
 
-function loadData() {
+function loadData(window) {
   APIData.loadAll(function(index, length) {
     var percent = index/length;
-    mainWindow.webContents.send('updateProgressBar', percent);
+    window.webContents.send('updateProgressBar', percent);
   }).then(function() {
-    mainWindow.webContents.send('finishedLoading', true);
+    window.webContents.send('finishedLoading', true);
   }, function(error) {
     if(error === 'offline') {
-      mainWindow.webContents.send('offline');
+      window.webContents.send('offline');
     } else {
       console.log('Error: ' + error); //TODO: Handle errors better.
     }
@@ -301,49 +357,3 @@ function loadData() {
     console.log('Exception: ' + error); //TODO: Handle errors better.
   });
 }
-
-// The methods below will be called upon receiving various messages from our
-// rendering thread. These are used to do things in the app when the user clicks
-// on things in the interface.
-ipcMain.on('close-main-window', function() {
-  app.quit();
-});
-
-ipcMain.on('minimize-main-window', function() {
-  mainWindow.minimize();
-});
-
-ipcMain.on('maximize-main-window', function() {
-  (mainWindow.isMaximized() ? mainWindow.unmaximize() : mainWindow.maximize()); // jshint ignore:line
-});
-
-ipcMain.on('openChampSelect', function() {
-  openChampSelectWindow();
-});
-
-ipcMain.on('retrieveChamps', function() {
-  if(!champSelectWindow) {return;}
-  champSelectWindow.webContents.send('champions', {champs: APIData.champs, versions: APIData.versionData, groups: require('./API/groups.json'), selected: APIData.champKeys});
-});
-
-ipcMain.on('newChampSelection', function(event, champs) {
-  if(!champs) {return;}
-  APIData.champKeys = champs;
-  mainWindow.webContents.send('champsChanged');
-});
-
-ipcMain.on('closeChampsWindow', function() {
-  if(!champSelectWindow) {return;}
-  champSelectWindow.close();
-});
-
-ipcMain.on('reloadData', function() {
-  loadData();
-});
-
-ipcMain.on('openURL', function(event, message) {
-  var open = require('open');
-  if(message) {
-    open(message);
-  }
-});
